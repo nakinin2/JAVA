17c75ad470f4e108d7cdcf70310d672fe1a69662
JJONAMI <nickelcomlex@gmail.com>
2018-09-17 10:17:20 +0900

    p0917 update message

--- p0917/src/p0917/helloWorld.java
+++ p0917/src/p0917/helloWorld.java
@@ -4,7 +4,9 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("HelloWorld!!!");
+		System.out.println("헬로 월드!!!");
+		System.out.println("헬로 월드22!!!");
 	}
 
 }
